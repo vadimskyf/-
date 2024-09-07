16ab7d1f24494d90a530ccfa6a4678945e08b0ef
vadimskyf <v5533vadimn@gmail.com>
2024-09-07 12:21:01 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+emojis = ["^^", "0_o", ":)", "¯\\_(ツ)_/¯", "(￢_￢)"]
+my_words = ["кодить", "спать люблю", "люблю играть"] 
+print(random.choice(my_words))
+print(random.choice(emojis))
+name = input()
+print("Hi ", name)
